5e2399c888d439fdc96049a3acde8d461858ce62
Daniela Rodríguez <80016080+danielarodriguez4@users.noreply.github.com>
2024-11-13 14:21:12 -0500

    Update start.sh
    Actualización para sincronización con fase 3

--- Fase 2/scripts/start.sh
+++ Fase 2/scripts/start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
 if [[ "$1" == "train" ]]; then
+    # Ejecuta el script de entrenamiento
     python train.py
 elif [[ "$1" == "predict" ]]; then
+    # Ejecuta el script de predicción
     python predict.py
+elif [[ "$1" == "api" ]]; then
+    # Inicia el servidor Flask para la API REST
+    echo "Iniciando la API REST Flask..."
+    export FLASK_APP=apirest.py
+    export FLASK_ENV=development  # Solo en desarrollo, quítalo para producción
+    flask run --host=0.0.0.0 --port=5000
 else
-    echo "Opción inválida. Utiliza 'train' o 'predict'"
+    echo "Opción inválida. Utiliza 'train', 'predict' o 'api'."
+fi
